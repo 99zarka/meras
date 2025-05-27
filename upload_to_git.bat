42d1749cf1af22ed007f7a28ad6f304328955aa3
99zarka <mohamedmanchestrawy@gmail.com>
2025-05-27 11:32:14 +0300

    Sign in page

--- upload_to_git.bat
+++ upload_to_git.bat
@@ -5,6 +5,7 @@ echo Enter commit message:
 set /p commit_message="Commit message: "
 git commit -m "%commit_message%"
 git branch -M main
+git remote add origin https://github.com/99zarka/meras.git
 git remote set-url origin https://github.com/99zarka/meras.git
 git push -u origin main
 pause
