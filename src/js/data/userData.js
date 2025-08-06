dab3b13e0a1dc7b9029edeeea970a38d1c07dc2d
99zarka <mohamedmanchestrawy@gmail.com>
2025-08-06 19:15:23 +0300

    path problem

--- src/js/data/userData.js
+++ src/js/data/userData.js
@@ -3,7 +3,7 @@ export function getUserData() {
         firstName: "محمد",
         lastName: "جلال",
         fullName: "محمد جلال",
-        profilePicture: '../src/images/profilepic.jpg',
+        profilePicture: './src/images/profilepic.jpg',
         lastUpdate: "2025-01-01T12:00:00Z",
     };
 }
